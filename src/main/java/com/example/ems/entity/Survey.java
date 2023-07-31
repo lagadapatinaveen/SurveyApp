83ff62dc98e0f8b53e0acd3aa5cec07727e2c386
naveen <naveen201699@gmail.com>
2023-07-31 01:37:19 -0400

    "SurveyApplication Intial commit"

---
+++ src/main/java/com/example/ems/entity/Survey.java
@@ -0,0 +1,21 @@
+package com.example.ems.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="survey")
+public class Survey {
+    @Id
+    @GeneratedValue
+    private int sid;
+    private String fname;
+    private String lname;
+    private String city;
+}
